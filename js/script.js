37d2a4f9437be21961969ee4a280e5affb564550
bereketwag <wagawbereket@gmail.com>
2024-09-13 11:55:34 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// js/script.js
+const toggleButton = document.getElementById('mobile-menu-toggle');
+const mobileMenu = document.getElementById('mobile-menu');
+
+toggleButton.addEventListener('click', () => {
+  mobileMenu.classList.toggle('hidden');
+});
